7a875dcf9280ab8030492254b28aa56ef8960d17
William Lam <zcabwhy@ucl.ac.uk>
2017-02-20 16:53:42 +0000

    fixed graph.py

--- PythonFiles/Graph/graph.py
+++ PythonFiles/Graph/graph.py
@@ -4,18 +4,21 @@ def graph(polygons,robots):
     polys = [];
     temp = [];
     shortest = [];
+    z = len(robots) - 1
     for x in polygons:
         for a,b in x:
-            print a, "then", b
+            # print a, "then", b
             temp.append(vg.Point(a,b));
         polys.append(temp);
+        temp = [];
+    # print polys
     g = vg.VisGraph()
     g.build(polys)
     shortest = g.shortest_path((vg.Point(robots[0][0],robots[0][1])), (vg.Point(robots[1][0],robots[1][1])))
-    for i in robots:
-        if len(robots) > i:
-            print i
-            shortest += g.shortest_path(shortest, (vg.Point(robots[i][0],robots[i][1])))
+    for i in xrange(1, len(robots) - 1, 1):
+            # print i
+            shortest.pop()
+            shortest += g.shortest_path((vg.Point(robots[i][0],robots[i][1])), (vg.Point(robots[i+1][0],robots[i+1][1])) )
     print shortest
 
-graph([[(1,2),(1,4),(3,4),(3,2)]],[(0,1),(2,0),(3,4)])
+graph([[(1,2),(1,4),(3,4),(3,2)],[(8,1),(4,1),(4,4),(5,2)]],[(0,1),(2,0),(3,5),(6,2),(9,0)])
