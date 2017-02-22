5da5b3b0bd5b5ccfdafdb11fde6d1d5f7f45d094
Sam Pham <sam.h.pham@gmail.com>
2017-02-22 12:06:41 +0000

    RRT batch files

--- Parser/PY Files/1.py
+++
@@ -1,6 +0,0 @@
-import pyvisgraph as vg
-polys = [[vg.Point(1,6),vg.Point(1,1),vg.Point(5,1),vg.Point(5,5),vg.Point(3,5),vg.Point(3,3),vg.Point(4,3),vg.Point(4,2),vg.Point(2,2),vg.Point(2,6),vg.Point(6,6),vg.Point(6,0),vg.Point(0,0),vg.Point(0,6)]]
-g = vg.VisGraph()
-g.build(polys)
-shortest = g.shortest_path(vg.Point(-1,-1), vg.Point(4,4))
-print shortest
